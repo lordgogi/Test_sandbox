db0590dafcd21b68437aa26458f2f139d4b5f26e
Jakub <30927542+lordgogi@users.noreply.github.com>
2018-11-29 14:22:16 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+
